a79f05d9f62680bfd18e6f1320f0d11cb8547a8d
HicaroD <hdanrlley1@gmail.com>
2022-12-11 07:42:25 -0300

    neovim: setting tab navigation

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -7,6 +7,10 @@ local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
 local term_opts = { silent = true }
 
+map("", "<Space>", "<Nop>", {})
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
 -- Nvim-tree
 map("n", "<C-p>", ":NvimTreeToggle<CR>", {})
 
@@ -15,6 +19,13 @@ map("n", "<C-f>", "<cmd>lua require'telescope.builtin'.find_files(require('teles
 
 -- Comment lines
 map("x", "<C-c>", ":'<,'>CommentToggle<CR>", {}) -- Comment Multiple Lines In Visual Mode
+
+-- Tab navigation
+map("n", "<Leader>tn", ":tabnew<cr>", {})
+map("n", "<Leader>tc", ":tabclose<cr>", {})
+map("n", "<Leader>to", ":tabonly<cr>", {})
+map("n", "<C-h>", ":tabprevious<cr>", {})
+map("n", "<C-l>", ":tabnext<cr>", {})
 
 -- Built-in auto pairs
 vim.cmd([[
