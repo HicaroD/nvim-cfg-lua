7719d7c75cccf1bbd10a3d6199d24585026fd281
HicaroD <hdanrlley1@gmail.com>
2023-06-12 20:23:01 -0300

    feat(nvim): format on save for Golang

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -28,13 +28,13 @@ lsp.nvim_workspace()
 local cmp = require("cmp")
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+  ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+  ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+  ["<Tab>"] = cmp.mapping.confirm({ select = true }),
   ["<C-Space>"] = cmp.mapping.complete(),
 })
 
-cmp_mappings['<S-Tab>'] = nil
+cmp_mappings["<S-Tab>"] = nil
 
 lsp.setup_nvim_cmp({
   mapping = cmp_mappings
@@ -43,16 +43,16 @@ lsp.setup_nvim_cmp({
 lsp.set_preferences({
     suggest_lsp_servers = false,
     sign_icons = {
-        error = 'E',
-        warn = 'W',
-        hint = 'H',
-        info = 'I'
+        error = "E",
+        warn = "W",
+        hint = "H",
+        info = "I"
     }
 })
 
 lsp.on_attach(function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
-  vim.keymap.set('n', "<leader>d", "<cmd>tab split | lua vim.lsp.buf.definition()<cr>", { noremap = true, silent = true })
+  vim.keymap.set("n", "<leader>d", "<cmd>tab split | lua vim.lsp.buf.definition()<cr>", { noremap = true, silent = true })
   vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
   vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
   vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
@@ -71,17 +71,18 @@ lsp.format_on_save({
     timeout_ms = 250,
   },
   servers = {
-    ['tsserver'] = {'typescript'},
-    ['rust_analyzer'] = {'rust'},
-    ['dartls'] = {'dart'},
-    ['black'] = {'python'},
+    ["tsserver"] = {"typescript"},
+    ["rust_analyzer"] = {"rust"},
+    ["dartls"] = {"dart"},
+    ["black"] = {"python"},
+    ["gopls"] = {"go"},
   }
 })
 
 lsp.setup()
 
-local null_ls = require('null-ls')
-local null_opts = lsp.build_options('null-ls', {})
+local null_ls = require("null-ls")
+local null_opts = lsp.build_options("null-ls", {})
 
 null_ls.setup({
   on_attach = function(client, bufnr)
