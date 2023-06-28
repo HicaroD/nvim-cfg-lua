45e745d83d19b7bb9454cbab920ab475aa1875b5
HicaroD <hdanrlley1@gmail.com>
2023-06-28 09:10:16 -0300

    nvim(treesitter): adding python to the list of ensured installed

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc", "sql", "typescript", "rust", "java", "javascript", "prisma", "yaml", "dart", "gitcommit", "go" },
+  ensure_installed = { "c", "python", "cpp", "lua", "vim", "vimdoc", "sql", "typescript", "rust", "java", "javascript", "prisma", "yaml", "dart", "gitcommit", "go" },
   sync_install = false,
   auto_install = false,
   highlight = {
