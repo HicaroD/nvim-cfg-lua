4da23e2c2b6193ddb8e53f5b6960b8000c4b2dcb
HicaroD <hdanrlley1@gmail.com>
2023-07-09 12:19:42 -0300

    nvim: configuring method for setting colorscheme properly

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -1,7 +1,8 @@
-function setupVscode()
+function setupVscode(transparent)
   local c = require('vscode.colors').get_colors()
   require('vscode').setup({
     disable_nvimtree_bg = true,
+    transparent = transparent,
     color_overrides = {
       vscBack = "#181818"
     },
@@ -9,14 +10,29 @@ function setupVscode()
   require("vscode").load()
 end
 
-function setColorscheme(colorscheme)
-  vim.o.termguicolors = true
-  local colorscheme_name = colorscheme or "vscode"
 
-  if colorscheme == "vscode" then
-    setupVscode()
-  end
-  vim.cmd("colorscheme " .. colorscheme_name)
+function setupGithub(transparent)
+  require('github-theme').setup({
+    options = {
+      transparent = transparent,
+    },
+  })
+  vim.cmd("colorscheme " .. "github_dark_dimmed")
 end
 
-setColorscheme("github_dark_dimmed")
+function setColorscheme(args)
+  vim.o.termguicolors = true
+
+  local colorscheme = args.name
+  local transparent = args.transparent
+
+  if colorscheme == "vscode" then
+    setupVscode(transparent)
+  elseif colorscheme == "github" then
+    setupGithub(transparent)
+  else
+    vim.cmd("colorscheme " .. colorscheme_name)
+  end
+end
+
+setColorscheme { name="github", transparent=false }
