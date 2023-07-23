9110de2269e00768dac8f97a9d3405a7be11f01d
HicaroD <hdanrlley1@gmail.com>
2023-07-23 15:06:45 -0300

    nvim(nvim-tree): changing some keybindings

--- after/plugin/nvimtree.lua
+++ after/plugin/nvimtree.lua
@@ -8,51 +8,68 @@ end
 
 local tree_cb = nvim_tree_config.nvim_tree_callback
 
+local function my_on_attach(bufnr)
+  local api = require("nvim-tree.api")
+
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+  api.config.mappings.default_on_attach(bufnr)
+  vim.keymap.set('n', '<space>', api.node.open.edit, opts("Open"))
+end
+
 nvimtree.setup({
-    update_focused_file = {
-        enable = true,
-        update_cwd = true,
-    },
-    renderer = {
-        root_folder_modifier = ":t",
-        icons = {
-            glyphs = {
-                default = "",
-                symlink = "",
-                folder = {
-                    arrow_open = "",
-                    arrow_closed = "",
-                    default = "",
-                    open = "",
-                    empty = "",
-                    empty_open = "",
-                    symlink = "",
-                    symlink_open = "",
-                },
-                git = {
-                    unstaged = "",
-                    staged = "S",
-                    unmerged = "",
-                    renamed = "➜",
-                    untracked = "U",
-                    deleted = "",
-                    ignored = "◌",
-                },
-            },
+  on_attach = my_on_attach,
+  update_focused_file = {
+    enable = true,
+    update_cwd = true,
+  },
+  renderer = {
+    root_folder_modifier = ":t",
+    icons = {
+      glyphs = {
+        default = "",
+        symlink = "",
+        folder = {
+          arrow_open = "",
+          arrow_closed = "",
+          default = "",
+          open = "",
+          empty = "",
+          empty_open = "",
+          symlink = "",
+          symlink_open = "",
         },
-    },
-    diagnostics = {
-        enable = true,
-        show_on_dirs = true,
-        icons = {
-            hint = "",
-            info = "",
-            warning = "",
-            error = "",
+        git = {
+          unstaged = "",
+          staged = "S",
+          unmerged = "",
+          renamed = "➜",
+          untracked = "U",
+          deleted = "",
+          ignored = "◌",
         },
+      },
     },
-    view = {
-        width = 30,
-        side = "left",
+  },
+  diagnostics = {
+    enable = true,
+    show_on_dirs = true,
+    icons = {
+      hint = "",
+      info = "",
+      warning = "",
+      error = "",
     },
+  },
+  view = {
+    width = 30,
+    side = "left",
+  },
+  renderer = {
+    highlight_opened_files = "name",
+    indent_markers = {
+      enable = true,
+    }
+  }
 })
