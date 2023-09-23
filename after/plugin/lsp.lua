704af3c3ce0948a34f738d9d49157d7ddcf04b33
HicaroD <hdanrlley1@gmail.com>
2023-09-23 00:26:44 -0300

    fix(docs): wrong comment about the comment for organizing imports

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -75,5 +75,5 @@ vim.api.nvim_create_autocmd("User", {
   desc = "Update signature help on jump placeholder",
 })
 
--- `:OR` command for organizing imports of the current buffer
+-- `:OI` command for organizing imports of the current buffer
 vim.api.nvim_create_user_command("OI", "call CocActionAsync('runCommand', 'editor.action.organizeImport')", {})
