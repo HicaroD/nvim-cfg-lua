c5b0fa35eb5a6fe62f32d16759fcba6115c977be
HicaroD <hdanrlley1@gmail.com>
2023-10-09 15:11:29 -0300

    refactor(remaps): binding `<leader>c` to command `:q`

--- lua/hicaro/remaps.lua
+++ lua/hicaro/remaps.lua
@@ -20,20 +20,21 @@ vim.keymap.set("n", "<leader>Y", [["+Y]])
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- Use <leader>c for closing current tab buffer
-vim.keymap.set("n", "<leader>c", function()
-  local buffers = vim.fn.getbufinfo({ buflisted = 1 })
+vim.keymap.set("n", "<leader>c", ":q<CR>")
+-- vim.keymap.set("n", "<leader>c", function()
+  -- local buffers = vim.fn.getbufinfo({ buflisted = 1 })
 
-  local number_of_buffers = 0
-  for _ in pairs(buffers) do
-    number_of_buffers = number_of_buffers + 1
-  end
+  -- local number_of_buffers = 0
+  -- for _ in pairs(buffers) do
+  --   number_of_buffers = number_of_buffers + 1
+  -- end
 
-  if number_of_buffers == 1 then
-    vim.api.nvim_command("bd!")
-  else
-    vim.api.nvim_command("q")
-  end
-end)
+  -- if number_of_buffers == 1 then
+  --   vim.api.nvim_command("bd!")
+  -- else
+  --   vim.api.nvim_command("q")
+  -- end
+-- end)
 
 -- NOTE: This code below is useful for when I'm using bufferline plugin
 -- vim.keymap.set("n", "<leader>c", function()
