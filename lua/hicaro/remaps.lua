535050de17f35b1c0f433361ba35cf7f714cf3f8
HicaroD <hdanrlley1@gmail.com>
2023-11-06 23:16:12 -0300

    feat(statusline): adding git branch to default statusline

--- lua/hicaro/remaps.lua
+++ lua/hicaro/remaps.lua
@@ -1,10 +1,6 @@
 -- Leader key to ','
 vim.g.mapleader = ","
 
--- Move lines selected in visual mode with J or K
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
 -- Take the line below and append to the current line with J
 vim.keymap.set("n", "J", "mzJ`z")
 
--- lua/hicaro/statusline.lua
+++ lua/hicaro/statusline.lua
@@ -1,4 +1,15 @@
 -- NOTE: using Lualine
 vim.cmd([[
-set statusline=\[%{&ff}:%{&fenc}:%Y]\ %<%f%m\ \ \[%{strftime('%Y/%b/%d\ %a\ %I:%M\ %p')}\]\ %=\ %l\/%L\ %c%V\ %P
+fun! GitBranch(file)
+    let l:dir = fnamemodify(system('readlink -f ' . a:file), ':h')
+    let l:cmd = 'git -C ' . l:dir . ' branch --show-current 2>/dev/null'
+    let b:git_current_branch = trim(system(l:cmd))
+endfun
+
+augroup GitBranch
+    autocmd!
+    autocmd BufEnter,ShellCmdPost,FileChangedShellPost * call GitBranch(expand('%'))
+augroup END
+
+set statusline=\[%{&ff}:%{&fenc}:%Y]\ %<%f%m\ \ \[%{strftime('%Y/%b/%d\ %a\ %I:%M\ %p')}\]\ \ %=\ %l\/%L\ %c%V\ %P\ %32(%=%{b:git_current_branch}%)
 ]])
