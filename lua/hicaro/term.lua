82144a60a7f29f587ee6a648f2beb9d62199d0de
HicaroD <hdanrlley1@gmail.com>
2023-11-07 16:19:19 -0300

    feat(term): using `<C-w>` for going to Normal mode in terminal

--- lua/hicaro/term.lua
+++ lua/hicaro/term.lua
@@ -3,11 +3,12 @@ vim.cmd([[
 " Splitting terminal at the bottom of the screen
 set splitbelow
 
-" Exit buffer when the terminal is closed
+" Exit buffer when the terminal is closed (using commands like "exit", for example)
 autocmd TermClose * if !v:event.status | exe 'bdelete! '..expand('<abuf>') | endif
 
 " Go directly to INSERT mode when opening the terminal
 autocmd TermOpen * startinsert
+
 " Using <ESC> for going to NORMAL mode in terminal
-tnoremap <Esc> <C-\><C-n>
+tnoremap <C-w> <C-\><C-n>
 ]])
