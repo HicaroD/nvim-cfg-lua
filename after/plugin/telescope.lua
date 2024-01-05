9a914586ac98d21261ddd0eca9ff9e632d192435
HicaroD <hdanrlley1@gmail.com>
2024-01-05 08:13:20 -0300

    feat(telescope): increase vertical width and removing file preview

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -10,6 +10,10 @@ end
 local telescope = require("telescope")
 telescope.setup({
   defaults = {
+    layout_config = {
+      horizontal = { width = 0.9 }
+    },
+    preview = false,
     mappings = {
       i = {
         ["<cr>"] = function(bufnr)
