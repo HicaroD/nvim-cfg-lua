c2a2b5c2aab02715213a5b7b3093443cb520b355
HicaroD <hdanrlley1@gmail.com>
2024-01-16 21:44:44 -0300

    chore: removing lazy.nvim code

--- lua/hicaro/plugins.lua
+++ lua/hicaro/plugins.lua
@@ -69,24 +69,3 @@ return packer.startup(function(use)
     require("packer").sync()
   end
 end)
-
--- Lazy
--- local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
--- if not vim.loop.fs_stat(lazypath) then
---   vim.fn.system({
---     "git",
---     "clone",
---     "--filter=blob:none",
---     "--single-branch",
---     "https://github.com/folke/lazy.nvim.git",
---     lazypath,
---   })
--- end
--- vim.opt.runtimepath:prepend(lazypath)
---
--- local lazy = require("lazy")
--- lazy.setup(plugins, {
---   performance = {
---     cache = { enabled = true },
---   },
--- })
