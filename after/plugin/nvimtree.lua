1666f6c39b4dd80218393ea6989fdd81868f48d8
HicaroD <hdanrlley1@gmail.com>
2024-01-28 14:56:36 -0300

    fix(nvimtree): removing some icons (already in the default config)

--- after/plugin/nvimtree.lua
+++ after/plugin/nvimtree.lua
@@ -24,31 +24,6 @@ nvimtree.setup({
   },
   renderer = {
     root_folder_modifier = ":t",
-    icons = {
-      glyphs = {
-        default = "",
-        symlink = "",
-        folder = {
-          arrow_open = "",
-          arrow_closed = "",
-          default = "",
-          open = "",
-          empty = "",
-          empty_open = "",
-          symlink = "",
-          symlink_open = "",
-        },
-        git = {
-          unstaged = "",
-          staged = "S",
-          unmerged = "",
-          renamed = "➜",
-          untracked = "U",
-          deleted = "",
-          ignored = "◌",
-        },
-      },
-    },
     highlight_opened_files = "name",
     indent_markers = {
       enable = true,
