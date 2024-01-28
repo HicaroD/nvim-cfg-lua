30bef2a6657bff619ec41c661bdfe946488c5731
HicaroD <hdanrlley1@gmail.com>
2024-01-28 14:54:19 -0300

    fix(lsp): translating comment to english

--- after/plugin/lsp/keybindings.lua
+++ after/plugin/lsp/keybindings.lua
@@ -10,7 +10,7 @@ utils.keyset("n", "]d", "<Plug>(coc-diagnostic-next)", { silent = true })
 local opts = { silent = true, noremap = true, expr = true, replace_keycodes = false }
 utils.keyset("i", "<TAB>", [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], opts)
 
--- Renomear
+-- Renaming
 function _G.check_back_space()
   local col = vim.fn.col(".") - 1
   return col == 0 or vim.fn.getline("."):sub(col, col):match("%s") ~= nil
