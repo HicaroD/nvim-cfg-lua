1ba4453fc2af3cef4550e8e2ed68c86c2ee7fdd5
HicaroD <hdanrlley1@gmail.com>
2024-02-20 21:07:10 -0300

    fix(bufdel): formatting config

--- after/plugin/bufdel.lua
+++ after/plugin/bufdel.lua
@@ -1,12 +1,13 @@
 local utils = require("hicaro.utils")
 
-  local ok = utils.prequire("bufdel")
-  if not ok then
-    print("Unable to import bufdel module")
-    return
-  end
+local ok = utils.prequire("bufdel")
+if not ok then
+  print("Unable to import bufdel module")
+  return
+end
 
-  local bufdel = require("bufdel")
-  bufdel.setup({
-    quit = false,
-  })
+local bufdel = require("bufdel")
+bufdel.setup({
+  quit = false,
+})
+
