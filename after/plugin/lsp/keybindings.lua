3fcdf4204e7f66806aa6a3febd695dd883663ed9
HicaroD <hdanrlley1@gmail.com>
2024-02-22 09:53:46 -0300

    feat(lsp): keybinding for renaming current file and update references

--- after/plugin/lsp/keybindings.lua
+++ after/plugin/lsp/keybindings.lua
@@ -30,3 +30,7 @@ function _G.show_docs()
   end
 end
 utils.keyset("n", "K", "<CMD>lua _G.show_docs()<CR>", { silent = true })
+
+
+-- Rename current file and update all references
+utils.keyset("n", "<leader>rf", ":CocCommand workspace.renameCurrentFile<CR>")
