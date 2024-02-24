0a025365fe14c564953bde2c9525fe3a1c756fcb
HicaroD <hdanrlley1@gmail.com>
2024-02-24 15:07:53 -0300

    feat: bash script for installing all plugins and setting up CoC

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+nvim -c ":PackerSync"
+
+nvim -c ":CocInstall \
+  coc-java \
+  coc-html \
+  coc-css \
+  coc-tsserver \
+  coc-flutter-tools \
+  coc-go \
+  coc-clangd \
+  coc-pyright \
+  coc-markdownlint \
+  coc-sumneko-lua \
+  coc-rust-analyzer"
+
+nvim -c ":CocInstall \
+  coc-json \
+  coc-prisma \
+  coc-xml \
+  coc-floatinput \
+  coc-snippets \
+  @yaegassy/coc-tailwindcss3 \
+  coc-emmet \
+  coc-prettier \
+  coc-sh"
