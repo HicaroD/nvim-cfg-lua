91811407013673e63f86b329f50c515caec0d692
HicaroD <hdanrlley1@gmail.com>
2024-02-24 15:13:53 -0300

    feat: adding command for installing Packer

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
 nvim -c ":PackerSync"
 
 nvim -c ":CocInstall \
