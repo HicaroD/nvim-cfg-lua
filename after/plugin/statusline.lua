8b8b1970beb21d0fbffd4ceb5c73fc305c15f4f5
HicaroD <hdanrlley1@gmail.com>
2024-02-29 17:49:50 -0300

    statusline: changing separator character

--- after/plugin/statusline.lua
+++ after/plugin/statusline.lua
@@ -9,7 +9,7 @@ end
 local lualine = require("lualine")
 lualine.setup({
   options = {
-    component_separators = "┃",
+    component_separators = "│",
     section_separators = "",
   },
   sections = {
