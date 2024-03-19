585619d55fc129b5fff0e2d3f175a254e17bc9a8
HicaroD <hdanrlley1@gmail.com>
2024-03-19 08:59:34 -0300

    nvim: updating deps and simplifying colorscheme config

--- lua/hicaro/plugins/colorscheme.lua
+++ lua/hicaro/plugins/colorscheme.lua
@@ -1,49 +1,16 @@
-local utils = require("hicaro.utils")
-
 return {
   "Mofiqul/vscode.nvim",
   config = function()
-    local function setupVscode(transparent)
-      local ok = utils.prequire("vscode")
-      if not ok then
-        print("Unable to import VSCode colorscheme")
-        return
-      end
-      local vscode = require("vscode")
-
-      vscode.setup({
-        disable_nvimtree_bg = true,
-        transparent = transparent,
-        color_overrides = {
-          vscBack = "#181818",
-        },
-      })
-      vscode.load()
-    end
-
-    local function setColorscheme(args)
-      vim.o.termguicolors = true
-
-      local colorscheme = args.name
-      local transparent = args.transparent
-      local dark = args.dark
-
-      if colorscheme == "nocolors" then
-        return
-      end
-
-      if colorscheme == "vscode" then
-        setupVscode(transparent)
-      else
-        print("No colorscheme were set")
-        return
-      end
-
-      if dark == "true" then
-        vim.cmd("set background=dark")
-      end
-    end
-
-    setColorscheme({ name = "vscode", transparent = false, dark = "true" })
+    vim.o.termguicolors = true
+    local vscode = require("vscode")
+    vscode.setup({
+      disable_nvimtree_bg = true,
+      transparent = false,
+      color_overrides = {
+        vscBack = "#181818",
+      },
+    })
+    vscode.load()
+    vim.cmd("colorscheme vscode")
   end,
 }
