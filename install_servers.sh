76e8aaeab195de3a7464f4359a08d232f7fa7514
HicaroD <hdanrlley1@gmail.com>
2024-04-23 09:42:40 -0300

    feat: script for installing all necessary LSP

---
+++ install_servers.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+declare -a servers=(
+  "coc-clangd"        # C / C++ / Objective-C
+  "coc-rust-analyzer" # Rust
+  "coc-tsserver"      # Javascript / Typescript
+  "coc-java"          # Java
+  "coc-go"            # Golang
+  "coc-pyright"       # Python
+  "coc-markdownlint"  # Markdown
+  "coc-html"          # HTML
+  "coc-css"           # CSS
+  "coc-emmet"         # Emmet
+)
+
+for i in "${servers[@]}"
+do
+   echo "Installing language server: $i"
+   COMMAND=""
+   nvim --headless +"CocInstall -sync $i" +"qall"
+done
