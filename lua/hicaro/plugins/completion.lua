4044313985b0fbfc81c11715439eca1f66c73b0b
HicaroD <hdanrlley1@gmail.com>
2024-07-15 08:51:01 -0300

    feat: using `mini.completion` for a minimal completion setup

--- lua/hicaro/plugins/completion.lua
+++ lua/hicaro/plugins/completion.lua
@@ -1,10 +1,39 @@
 return {
-  "echasnovski/mini.nvim",
+  "echasnovski/mini.completion",
   version = false,
   config = function()
     local utils = require("hicaro.utils")
 
     local completion = require("mini.completion")
-    completion.setup()
+    completion.setup({
+      delay = {
+        completion = 10^7,
+      },
+    })
+
+    -- pre-select the first item
+    vim.o.completeopt = "menu,menuone,noinsert"
+
+    local keycode = vim.keycode or function(x)
+      return vim.api.nvim_replace_termcodes(x, true, true, true)
+    end
+
+    local keys = {
+      ["cr"] = keycode("<cr>"),
+      ["tab"] = keycode("<TAB>"),
+      ["ctrl-y"] = keycode("<C-y>"),
+      ["ctrl-y_cr"] = keycode("<C-y><CR>"),
+    }
+
+    _G.tab_action = function()
+      if vim.fn.pumvisible() ~= 0 then
+        local item_selected = vim.fn.complete_info()["selected"] ~= -1
+        return item_selected and keys["ctrl-y"] or keys["ctrl-y_cr"]
+      else
+        return keys["cr"]
+      end
+    end
+
+    vim.keymap.set("i", "<TAB>", "v:lua._G.tab_action()", { expr = true })
   end,
 }
